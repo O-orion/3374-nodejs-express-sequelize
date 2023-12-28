9175d84cd8aae653a39fbc6ccc2923b1e031f10e
O-orion <lucas.lvtorres@gmail.com>
2023-12-28 17:53:38 -0300

    metodo update

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -15,6 +15,23 @@ class Controller {
         }
     }
 
+    async update(req, res) {
+        const { id } = req.params;
+        const dadosAtualizados = req.body;
+
+        try {
+            const isUpdate = await this.entidadeService.update(dadosAtualizados, Number(id)); 
+            
+            if (!isUpdate) {
+                return res.status(400).json({ mensagem: 'Erro ao atualizar registro' });
+            }
+
+            return res.status(200).json({ mensagem: 'Registro atualizado com sucesso'});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 }
 
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -9,6 +9,20 @@ class Service {
   async getAll () {
     return dataSource[this.modelo].findAll();
   }
+
+  async update (dadosAtualizados, id) {
+    const listaRegistroAtualizado = dataSource[this.modelo].update(dadosAtualizados, {
+      where: {
+        id: id
+      }
+    });
+
+    if (listaRegistroAtualizado[0] === 0 ) {
+      return false;
+    }
+
+    return true;
+  }
 }
 
 module.exports = Service;
